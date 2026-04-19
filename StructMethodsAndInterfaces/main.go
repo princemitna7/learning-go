1b3c132dde004bb73d90fe166f3bc6a8b4379350
Prince Mitna <oldguard1998@gmail.com>
2026-04-19 23:00:57 +0530

    Updated Struct, Methods and Interfaces code

--- StructMethodsAndInterfaces/main.go
+++ StructMethodsAndInterfaces/main.go
@@ -9,8 +9,44 @@ type Student struct {
 	grades map[string]int
 }
 
+type movie struct {
+	name   string
+	rating float32
+}
+
+type Rectangle struct {
+	length  int
+	breadth int
+}
+
+func (r Rectangle) area() int {
+	return r.length * r.breadth
+}
+
+func (r *Rectangle) incLength(n int) {
+	for i := 0; i < n; i++ {
+		r.length += i
+	}
+}
+
+type myall interface {
+	area()
+	incLength()
+}
+
 func main() {
 	var s Student
-
 	fmt.Printf("%+v", s)
+
+	m := movie{name: "ABCD"}
+	var m2 movie
+	fmt.Printf("%+v", m)
+	fmt.Printf("%+v", m2)
+
+	r := Rectangle{breadth: 10, length: 5}
+	fmt.Println(r.area())
+	fmt.Println(r)
+	r.incLength(7)
+	fmt.Println(r.area())
+	fmt.Println(r)
 }
